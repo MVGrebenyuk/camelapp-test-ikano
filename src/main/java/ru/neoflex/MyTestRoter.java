406b785b553dbb1b092d6c8b7794e5294c2ae1f1
mgrebeniuk <Maxim13parkour@gmail.com>
2021-07-02 13:52:03 +0300

    Done!

--- src/main/java/ru/neoflex/MyTestRoter.java
+++ src/main/java/ru/neoflex/MyTestRoter.java
@@ -1,30 +1,59 @@
 package ru.neoflex;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonDataFormat;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.support.DefaultMessage;
+import org.apache.camel.tooling.model.JsonMapper;
+import org.apache.camel.util.json.JsonObject;
 import org.springframework.stereotype.Component;
-import ru.neoflex.types.PostRequestType;
-import ru.neoflex.types.ResponseType;
+import ru.neoflex.types.PersonDTO;
 
 @Component
 public class MyTestRoter extends RouteBuilder {
+
     @Override
     public void configure() throws Exception {
+
+
 
         restConfiguration()
              .component("servlet")
              .bindingMode(RestBindingMode.auto);
 
-        rest()
-                .path("/ikanoTest")
-                .consumes("application/json")
-                .produces("application/json")
 
-                .post()
-                .type(PostRequestType.class) // Setting the request type enables Camel to unmarshal the request to a Java object
-                .outType(ResponseType.class) // Setting the response type enables Camel to marshal the response to JSON
-                .to("bean:postBean");
+        from("rest:post:ikanoTest?consumer=application/json")
+                .routeId("Convert JSON to XML")
+                .unmarshal().json(PersonDTO.class, PersonDTO.class)
+                //.to("log:?showBody=true")
+                .choice()
+                .when(exchange -> (((PersonDTO) exchange.getIn().getBody()).getType().equals("developer")))
+                .process(exchange -> {
+                    changeExchange(exchange);
+                })
+                .to("file:src/data/persons/developers")
+                .otherwise()
+                .process(exchange -> {
+                    changeExchange(exchange);
+                })
+                .to("file:src/data/persons/otherPersons");
+
+
 
     }
 
+    private void changeExchange(Exchange exchange) throws JsonProcessingException {
+        PersonDTO person = (PersonDTO) exchange.getIn().getBody();
+        XmlMapper mapper = new XmlMapper();
+        String xmlStr = mapper.writeValueAsString(person);
+        System.out.println("PRINT MAP: " + xmlStr);
+        DefaultMessage message = new DefaultMessage(exchange);
+        message.setBody(xmlStr);
+        message.setMessageId(person.getName());
+        exchange.setMessage(message);
     }
+
+}
